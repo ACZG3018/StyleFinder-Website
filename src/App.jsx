91a3f7b4bb43c4b68407732a91b9ec1336e666ea
ACZG3018 <ACZG3018@GMAIL.COM>
2026-03-14 03:59:32 -0400

    Name Changes

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,11 @@ function App() {
         <div className="header" style={{ alignItems: "center", backgroundColor: "#5A7D8C", display: "flex", flexDirection: "row", justifyContent: "space-between", padding: "1rem" }}>
           <h1 style={{ color: "#4A3A33" }}>Style Finder</h1>
           <button style={{ backgroundColor: "#D6CCC2", borderRadius: "5px", color: "#4A3A33", fontSize: "2rem", height: "30%", padding: "0.5rem 1rem", border: "none", cursor: "pointer" }}>
-            <a
-              // uncomment when repo is public
-              // href="https://github.com/mohammadeltawil0/Style-Finder/archive/refs/heads/main.zip" className="download-btn"
+            <a href="https://f005.backblazeb2.com/file/stylefinder-releases/app.apk"
               onClick={() => console.log("Press Download Button")}
               style={{ color: "#4A3A33", textDecoration: "none" }}
             >
-              Download
+              Download .Apk
             </a>
           </button>
         </div>
